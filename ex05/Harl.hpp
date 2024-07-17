ac6893747e87a853e3dec39eea3c42522515da86
Anthony Grimaldo Ramos <agrimald@car15s5.42barcelona.com>
2024-07-17 19:17:31 +0200

    ex05 finished

---
+++ ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 17:11:43 by agrimald          #+#    #+#             */
+/*   Updated: 2024/07/17 17:11:45 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+    public:
+        void    complain(std::string level);
+};
+
+int main();
+
+#endif
---
+++ ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 16:55:20 by agrimald          #+#    #+#             */
+/*   Updated: 2024/07/17 16:55:21 by agrimald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOW");
+    return (0);
+}
